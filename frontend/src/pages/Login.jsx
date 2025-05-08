65d9c3b223ba3f7f738e2d1fe476b9b29073a252
Krisha <krishatuladhar@gmail.com>
2025-05-08 13:48:23 +0545

    created login and register page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,109 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { SlSocialFoursqare } from "react-icons/sl";
+import TextInput from "../components/TextInput";
+import { useForm } from "react-hook-form";
+import { useDispatch } from "react-redux";
+import Loading from "../components/Loading";
+import CustomButton from "../components/CustomButton";
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    mode: "onChange",
+  });
+  const onSubmit = async (data) => {};
 
-export default Login
+  const [errMsg, setErrMsg] = useState("");
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const dispatch = useDispatch();
+  return (
+    <div className="bg-bgColor w-full h-[100vh] flex items-center justify-center p-6">
+      <div className="w-full md:w-2/3 h-fit lg:h-full 2xl:h-5/6 py-8 lg:py-0 flex bg-primary rounded-xl overflow-hidden shadow-xl">
+        {/* LEFT */}
+        <div className="w-full lg:w-1/2 h-full p-10 2xl:px-20 flex flex-col justify-center ">
+          <div className="w-full flex gap-2 items-center mb-6">
+            <div className="p-2 bg-[#065ad8] rounded text-white">
+              <SlSocialFoursqare />
+            </div>
+            <span className="text-2xl text-[#065ad8] font-bold">Flicksy</span>
+          </div>
+
+          <p className="text-ascent-1 text-base font-semibold">
+            Log in to your account
+          </p>
+          <span className="text-sm mt-2 text-ascent-2">Welcome back</span>
+
+          <form className="py-8 flex flex-col gap-5'onSubmit={handleSubmit(onSubmit)}">
+            <TextInput
+              name="email"
+              placeholder="email@example.com"
+              label="Email Address"
+              type="email"
+              register={register("email", {
+                required: "Email Address is required",
+              })}
+              styles="w-full rounded-full"
+              labelStyle="ml-2"
+              error={errors.email ? errors.email.message : ""}
+            />
+
+            <TextInput
+              name="password"
+              label="Password"
+              placeholder="Password"
+              type="password"
+              styles="w-full rounded-full"
+              labelStyle="ml-2"
+              register={register("password", {
+                required: "Password is required!",
+              })}
+              error={errors.password ? errors.password?.message : ""}
+            />
+            <Link
+              to="/reset-password"
+              className="text-sm text-right text-blue font-semibold"
+            >
+              Forgot Password ?
+            </Link>
+            {errMsg?.message && (
+              <span
+                className={`text-sm ${
+                  errMsg?.status == "failed"
+                    ? "text-[#f64949fe]"
+                    : "text-[#2ba150fe]"
+                } mt-0.5`}
+              >
+                {errMsg?.message}
+              </span>
+            )}
+
+            {isSubmitting ? (
+              <Loading />
+            ) : (
+              <CustomButton
+                type='submit'
+                containerStyles={`inline-flex justify-center rounded-md bg-blue px-8 py-3 text-sm font-medium text-white outline-none`}
+                title='Login'
+              />
+            )}
+          </form>
+
+          <p className="text-ascent-2 text-sm text-center">
+            Don't have an account?
+            <Link
+              to="/register"
+              className="text-[#065ad8] font-semibold ml-2 cursor-pointer"
+            >
+              Create Account
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,9 +1,148 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { SlSocialFoursqare } from "react-icons/sl";
+import TextInput from "../components/TextInput";
+import { useForm } from "react-hook-form";
+import { useDispatch } from "react-redux";
+import Loading from "../components/Loading";
+import CustomButton from "../components/CustomButton";
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+  const {
+    register,
+    handleSubmit,
+    getValues,
+    formState: { errors },
+  } = useForm({
+    mode: "onChange",
+  });
+  const onSubmit = async (data) => {};
 
-export default Register
+  const [errMsg, setErrMsg] = useState("");
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const dispatch = useDispatch();
+  return (
+    <div className="bg-bgColor w-full h-[100vh] flex items-center justify-center p-6">
+      <div className="w-full md:w-2/3 h-fit lg:h-full 2xl:h-5/6 py-8 lg:py-0 flex flex-row-reverse bg-primary rounded-xl overflow-hidden shadow-xl">
+        {/* LEFT */}
+        <div className="w-full lg:w-1/2 h-full p-10 2xl:px-20 flex flex-col justify-center ">
+          <div className="w-full flex gap-2 items-center mb-6">
+            <div className="p-2 bg-[#065ad8] rounded text-white">
+              <SlSocialFoursqare />
+            </div>
+            <span className="text-2xl text-[#065ad8] font-bold">Flicksy</span>
+          </div>
+
+          <p className="text-ascent-1 text-base font-semibold">
+           Create your account
+          </p>
+    
+          <form className="py-8 flex flex-col gap-5'onSubmit={handleSubmit(onSubmit)}">
+            <div className="w-full flex flex-col lg:flex-row gap-1 md:gap-2">
+              <TextInput
+                name="firstName"
+                label="First Name"
+                placeholder="First Name"
+                type="text"
+                styles="w-full"
+                register={register("firstName", {
+                  required: "First Name is required!",
+                })}
+                error={errors.firstName ? errors.firstName?.message : ""}
+              />
+
+              <TextInput
+                label="Last Name"
+                placeholder="Last Name"
+                type="lastName"
+                styles="w-full"
+                register={register("lastName", {
+                  required: "Last Name do no match",
+                })}
+                error={errors.lastName ? errors.lastName?.message : ""}
+              />
+            </div>
+            <TextInput
+              name="email"
+              placeholder="email@example.com"
+              label="Email Address"
+              type="email"
+              register={register("email", {
+                required: "Email Address is required",
+              })}
+              styles="w-full"
+              error={errors.email ? errors.email.message : ""}
+            />
+
+            <div className="w-full flex flex-col lg:flex-row gap-1 md:gap-2">
+              <TextInput
+                name="password"
+                label="Password"
+                placeholder="Password"
+                type="password"
+                styles="w-full"
+                register={register("password", {
+                  required: "Password is required!",
+                })}
+                error={errors.password ? errors.password?.message : ""}
+              />
+
+              <TextInput
+                label="Confirm Password"
+                placeholder="Password"
+                type="password"
+                styles="w-full"
+                register={register("cPassword", {
+                  validate: (value) => {
+                    const { password } = getValues();
+
+                    if (password != value) {
+                      return "Passwords do no match";
+                    }
+                  },
+                })}
+                error={
+                  errors.cPassword && errors.cPassword.type === "validate"
+                    ? errors.cPassword?.message
+                    : ""
+                }
+              />
+            </div>
+            {errMsg?.message && (
+              <span
+                className={`text-sm ${
+                  errMsg?.status == "failed"
+                    ? "text-[#f64949fe]"
+                    : "text-[#2ba150fe]"
+                } mt-0.5`}
+              >
+                {errMsg?.message}
+              </span>
+            )}
+
+            {isSubmitting ? (
+              <Loading />
+            ) : (
+              <CustomButton
+                type="submit"
+                containerStyles={`inline-flex justify-center rounded-md bg-blue px-8 py-3 text-sm font-medium text-white outline-none`}
+                title="Create Account"
+              />
+            )}
+          </form>
+
+          <p className="text-ascent-2 text-sm text-center">
+            Already has an account?
+            <Link
+              to="/login"
+              className="text-[#065ad8] font-semibold ml-2 cursor-pointer"
+            >
+              Login
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;

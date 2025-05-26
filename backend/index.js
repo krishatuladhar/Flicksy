b8f95328e0b872b29dbbc0689744374d3aa84a96
Krisha <krishatuladhar@gmail.com>
2025-05-26 22:42:39 +0545

    for deployment

--- backend/index.js
+++ backend/index.js
@@ -23,11 +23,28 @@ const PORT = 8800;
 dbConnection();
 
 app.use(helmet());
-app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
+
+
+const allowedOrigins = [
+  "https://flicksy-sigma.vercel.app",
+  "http://localhost:8800"
+];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  }
+}));
+
 
 app.use(morgan("dev"));
 app.use(router);
